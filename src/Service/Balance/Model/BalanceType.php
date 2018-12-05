b2abf879b9cd5be071ef8fa0ec6daa3d019d8d5d
Mikhail Densiov <mikhail.denisov@dev-ifc.com>
2018-12-05 21:04:07 +0300

    - add deposit balance type

--- src/Service/Balance/Model/BalanceType.php
+++ src/Service/Balance/Model/BalanceType.php
@@ -10,6 +10,7 @@ class BalanceType
     public const EXTERNAL = 'external';
     public const DEBT     = 'debt';
     public const CREDIT   = 'credit';
+    public const DEPOSIT   = 'deposit';
     public const BLACK_HOLE  = 'black_hole';
 
     public static function getValues() : array
@@ -17,6 +18,7 @@ class BalanceType
         return [
             self::CURRENT  => 'Текущий',
             self::EXTERNAL => 'Внешний',
+            self::DEPOSIT  => 'Депозитный',
             self::DEBT     => 'Долговой',
             self::CREDIT   => 'Кредитный',
             self::BLACK_HOLE => 'Черная дыра',
